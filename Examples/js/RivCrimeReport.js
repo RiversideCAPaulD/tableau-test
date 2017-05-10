a068c7e48d419e5a70b8327cfb2c96d28adcb267
RiversideCAPaulD <pderouen@riversideca.gov>
2017-05-10 14:31:17 -0700

    Update RivCrimeReport.js

--- Examples/js/RivCrimeReport.js
+++ Examples/js/RivCrimeReport.js
@@ -14,8 +14,8 @@
         }];
 
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "crimeFeed",
+            alias: "Riverside Crime",
             columns: cols
         };
 
@@ -47,7 +47,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "Riverside Crime"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
