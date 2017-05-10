05291f85a290119e685881a3a89a3a9845d15a74
RiversideCAPaulD <pderouen@riversideca.gov>
2017-05-10 13:56:51 -0700

    Add files via upload

---
+++ Examples/js/RivCrimeReport.js
@@ -0,0 +1,56 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "id",
+            dataType: tableau.dataTypeEnum.string
+        }
+		{
+            id: "blockAddress",
+            alias: "Block",
+            dataType: tableau.dataTypeEnum.string
+        }
+		];
+
+        var tableSchema = {
+            id: "crimeFeed",
+            alias: "Riverside CA Crime Report",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("http://riversideca.gov/transparency/data/dataset/jsonfull/27/Crime_Reports", function(resp) {
+            var feat = resp.features,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "id": feat[i].id,
+                    "blockAddress": feat[i].properties.mag,
+                    
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "Riverside Crime Report"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
