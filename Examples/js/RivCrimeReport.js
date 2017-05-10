4d25dce92b0cb00f7baae8e0366621446f2edcd9
RiversideCAPaulD <pderouen@riversideca.gov>
2017-05-10 15:01:45 -0700

    Update RivCrimeReport.js

--- Examples/js/RivCrimeReport.js
+++ Examples/js/RivCrimeReport.js
@@ -5,13 +5,29 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "_id",
+            id: "id",
             dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "mag",
+            alias: "magnitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "title",
+            alias: "title",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "lat",
+            alias: "latitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "lon",
+            alias: "longitude",
+            dataType: tableau.dataTypeEnum.float
         }];
 
         var tableSchema = {
-            id: "Crime",
-            alias: "Riverside Crime",
+            id: "crime",
+            alias: "Crime",
             columns: cols
         };
 
@@ -20,15 +36,18 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("http://riversideca.gov/transparency/data/dataset/json/27/Crime_Reports", function(resp) {
+        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
             var feat = resp.features,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "_id": feat[i].id,
-                    
+                    "id": feat[i].id,
+                    "mag": feat[i].properties.mag,
+                    "title": feat[i].properties.title,
+                    "lon": feat[i].geometry.coordinates[0],
+                    "lat": feat[i].geometry.coordinates[1]
                 });
             }
 
