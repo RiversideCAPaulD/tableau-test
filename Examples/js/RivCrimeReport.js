0bab5d3e0c5b06a8c5801bca1bd712f89c8b8d76
RiversideCAPaulD <pderouen@riversideca.gov>
2017-05-10 17:02:48 -0700

    Update RivCrimeReport.js

--- Examples/js/RivCrimeReport.js
+++ Examples/js/RivCrimeReport.js
@@ -5,7 +5,7 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "id",
+            id: "Type",
             dataType: tableau.dataTypeEnum.string
         }];
 
@@ -27,7 +27,7 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
+                    "Type": feat[i].Type,
                     
                 });
             }
