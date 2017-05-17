69f1cc2cfc11595419f856d1baffc01b035c656a
RiversideCAPaulD <pderouen@riversideca.gov>
2017-05-17 13:36:34 -0700

    Create rivcrime2.js

---
+++ Examples/js/rivcrime2.js
@@ -0,0 +1,52 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [
+		{ id : "npc",  alias : "Npc", dataType : tableau.dataTypeEnum.string }
+		];
+		
+		
+
+        var tableSchema = {
+            id: "riversideCrime",
+            alias: "Riverside Crime JSON entries",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://riversideca.gov/transparency/data/dataset/json/27", function(resp) {
+            var feat = resp.features,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    
+                    "npc": feat[i].npc,
+					
+					                    
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "Riverside Crime Report"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
